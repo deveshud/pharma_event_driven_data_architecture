3e0574630c59f51281f57971e23486c9f35891fd
HP <devesh.ud@gmail.com>
2026-02-23 13:46:34 +0530

    snowflake different databases created

---
+++ snowflake/dev_snowflake.sql
@@ -0,0 +1,124 @@
+CREATE OR REPLACE DATABASE PHARMA_PLATFORM_DEV;
+
+CREATE OR REPLACE SCHEMA PHARMA_PLATFORM_DEV.RAW;
+CREATE OR REPLACE SCHEMA PHARMA_PLATFORM_DEV.BRONZE;
+CREATE OR REPLACE SCHEMA PHARMA_PLATFORM_DEV.SILVER;
+CREATE OR REPLACE SCHEMA PHARMA_PLATFORM_DEV.GOLD;
+CREATE OR REPLACE SCHEMA PHARMA_PLATFORM_DEV.CI;
+
+
+CREATE OR REPLACE WAREHOUSE WH_PHARMA
+WAREHOUSE_SIZE = 'XSMALL'
+AUTO_SUSPEND = 60
+AUTO_RESUME = TRUE;
+
+-- re-creating storage integration recreates the external id which needs to be added in trust policy 
+
+-- CREATE OR REPLACE STORAGE INTEGRATION PHARMA_S3_INT
+--   TYPE = EXTERNAL_STAGE
+--   STORAGE_PROVIDER = S3
+--   ENABLED = TRUE
+--   STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::293180004298:role/snowflake_pharma_s3_access'
+--   STORAGE_ALLOWED_LOCATIONS = ('s3://pharma-platform-data-project/raw/');
+
+-- DESC INTEGRATION PHARMA_S3_INT;
+
+USE SCHEMA PHARMA_PLATFORM_DEV.RAW;
+
+CREATE OR REPLACE FILE FORMAT CSV_FF
+  TYPE = CSV
+  FIELD_DELIMITER = ','
+  SKIP_HEADER = 1
+  FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+  NULL_IF = ('', 'NULL', 'null')
+  DATE_FORMAT = 'YYYY-MM-DD'
+  ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE;
+
+CREATE OR REPLACE STAGE RAW_S3_STAGE
+STORAGE_INTEGRATION = PHARMA_S3_INT
+URL = 's3://pharma-platform-data-project/raw/'
+FILE_FORMAT = CSV_FF;
+
+LIST @RAW_S3_STAGE;
+
+CREATE OR REPLACE TABLE HCP_MASTER_RAW (
+  hcp_id           STRING,
+  specialty        STRING,
+  territory        STRING,
+  engagement_tier  STRING,
+  src_file         STRING,
+  load_ts          TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+);
+
+
+CREATE OR REPLACE TABLE PRODUCT_MASTER_RAW (
+  product_code     STRING,
+  brand_name       STRING,
+  therapy_area     STRING,
+  launch_date      DATE,
+  lifecycle_stage  STRING,
+  src_file         STRING,
+  load_ts          TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+);
+
+CREATE OR REPLACE TABLE RX_EVENTS_RAW (
+  rx_id        STRING,
+  rx_date      DATE,
+  product_code STRING,
+  hcp_id       STRING,
+  quantity     NUMBER(38,0),
+  days_supply  NUMBER(38,0),
+  region       STRING,
+  payer_type   STRING,
+  channel      STRING,
+  src_file     STRING,
+  load_ts      TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+);
+
+
+CREATE OR REPLACE PIPE PIPE_RX_EVENTS
+  AUTO_INGEST = TRUE
+AS
+COPY INTO RX_EVENTS_RAW (rx_id, rx_date, product_code, hcp_id, quantity, days_supply, region, payer_type, channel, src_file)
+FROM (
+  SELECT
+    $1,
+    TO_DATE($2, 'YYYY-MM-DD'),
+    $3, $4,
+    TO_NUMBER($5), TO_NUMBER($6),
+    $7, $8, $9,
+    METADATA$FILENAME
+  FROM @RAW_S3_STAGE/rx_events/
+)
+FILE_FORMAT = (FORMAT_NAME = CSV_FF);
+
+CREATE OR REPLACE PIPE PIPE_HCP_MASTER
+  AUTO_INGEST = TRUE
+AS
+COPY INTO HCP_MASTER_RAW (hcp_id, specialty, territory, engagement_tier, src_file)
+FROM (
+  SELECT
+    $1, $2, $3, $4,
+    METADATA$FILENAME
+  FROM @RAW_S3_STAGE/hcp_master/
+)
+FILE_FORMAT = (FORMAT_NAME = CSV_FF);
+
+
+
+CREATE OR REPLACE PIPE PIPE_PRODUCT_MASTER
+  AUTO_INGEST = TRUE
+AS
+COPY INTO PRODUCT_MASTER_RAW (product_code, brand_name, therapy_area, launch_date, lifecycle_stage, src_file)
+FROM (
+  SELECT
+    $1, $2, $3, TO_DATE($4, 'YYYY-MM-DD'), $5,
+    METADATA$FILENAME
+  FROM @RAW_S3_STAGE/product_master/
+)
+FILE_FORMAT = (FORMAT_NAME = CSV_FF);
+
+
+ALTER PIPE PIPE_HCP_MASTER REFRESH;
+ALTER PIPE PIPE_PRODUCT_MASTER REFRESH;
+ALTER PIPE PIPE_RX_EVENTS REFRESH;
